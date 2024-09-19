a1bb4bb26120b0f745ddc1dcbc22e11e3844db12
Jack Brewer <jack@jackbrewer.co.uk>
2024-09-19 09:31:01 +0100

    Add recommended export

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ import noMultiIconImports from "./rules/no-multi-icon-imports.js";
 export const rules = {
   "no-multi-icon-imports": noMultiIconImports,
 };
+
+export const configs = {
+  recommended: {
+    plugins: ["boxicon-imports"],
+    rules: {
+      "boxicon-imports/no-multi-icon-imports": "error",
+    },
+  },
+};
